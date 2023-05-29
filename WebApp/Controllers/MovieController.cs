79c4a42a692746dcec278041ae8e781f40342910
Mario <hardimario13@gmail.com>
2023-05-29 08:20:07 +0200

    Projekt

---
+++ WebApp/Controllers/MovieController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+using System.Data;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class MovieController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            IEnumerable<Movie> movie = null;
+
+            movie = Movierep.Get();
+
+            if (movie.Count() != 0)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, movie);
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.NotFound);
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            Movie movie = Movierep.Get(id);
+            if (movie != null)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, movie);
+            }
+            else
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.NotFound);
+            }
+        }
+
+        public HttpResponseMessage Post(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest, "Invalid ");
+            }
+            Movierep.AddMovie(movie);
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, "Successfully.");
+        }
+       
+
+        public HttpResponseMessage Put(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest, "Invalid");
+            }
+            Movie currentMovie = Movierep.Get(id);
+            if (currentMovie != null)
+            {
+                
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK, "Successfully updated.");
+            }
+            else
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.NotFound);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.BadRequest, "Invalid");
+            }
+            Movierep.Delete(id);
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, "Successfully deleted.");
+        }
+
+    }
+}
---
+++ WebApp/Models/Movierep.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Movierep
+    {
+        
+        
+            public static List<Movie> movies = new List<Movie>
+            {
+            new Movie(1,2,"Spiderman"),
+            new Movie(2,5 ,"Batman"),
+            new Movie(3,10,"Yes"),
+        };
+            public static IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+        public static Movie Get(int id)
+        {
+            return movies.Find(movie => movie.Id == id);
+        }
+        public static void AddMovie(Movie movie)
+        {
+            movies.Add(movie);
+        }
+
+        public static void Delete(int id)
+        {
+            movies.Remove(Get(id));
+        }
+    }
+
+}
